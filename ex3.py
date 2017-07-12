04bf6568daee44444eb7bd9659782dcb85a9a25d
adhunik13 <adhunik13@yahoo.com>
2017-07-13 00:25:19 +0530

    added first program

---
+++ ex3.py
@@ -0,0 +1,26 @@
+#using Math symbol
+print "TIME TO DO SOME CALCULATIONS "
+print
+print  "ADDITION(25+13)", 25.1+13.1
+print  "Subtraction(25-15)", 25.2-15.1 
+print  "divide(25/5)", 25.1/5.2
+print  "Multiplication(25*5)", 25.2*5.2
+print "Modulus(25%10)", 25.4%10.3
+print
+
+
+#complex clculations now
+print "TIME FOR SOME COMPLEX CALCULATIONS"
+
+print "Complex:", 2.1+3.2/4.3-5.3*8.3%10.4
+
+#relational symbols
+
+print "who is greater", 5.4>2.3
+print
+print
+print "who is smaller", 5.2<2.4
+
+ 
+
+ 
