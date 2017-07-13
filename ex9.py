7607736323f5adb3820371cc182e9e668330383f
adhunik13 <adhunik13@yahoo.com>
2017-07-13 23:20:42 +0530

    9th program

---
+++ ex9.py
@@ -0,0 +1,14 @@
+tabby_cat = "\tI'm Tabbed in "
+Persian_cat = "this line is \nsplitted in two"
+backlash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list
+\t* cat food
+\t* fishies
+\t* meow\n\t* cat"""
+
+print tabby_cat
+print Persian_cat
+print backlash_cat
+print fat_cat
