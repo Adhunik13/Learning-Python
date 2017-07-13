d535017c5302935a731ef4f31a5a69f67469de00
adhunik13 <adhunik13@yahoo.com>
2017-07-13 23:20:51 +0530

    10th program

---
+++ ex10.py
@@ -0,0 +1,11 @@
+print "how old are you?"
+age = raw_input()
+print "how tall are you"
+height = raw_input()
+
+
+print "so you are %r old and %r tall " %(age,height)
+
+
+
+#input from user age = raw_input()
