868c8d8f5dab49fb11602461189e723c7d13452b
adhunik13 <adhunik13@yahoo.com>
2017-07-13 21:56:21 +0530

    2nd program

---
+++ ex4.py
@@ -0,0 +1,11 @@
+cars = 100
+space_in_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars-drivers
+cars_driven = drivers
+
+
+print "there are", cars, "cars in town."
+print "there is ",space_in_car, "people space in car."
+print "driver available are",drivers
