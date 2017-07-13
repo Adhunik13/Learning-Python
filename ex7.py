352fc15bf72e38069df33c13edeee62ea7a37943
adhunik13 <adhunik13@yahoo.com>
2017-07-13 22:46:43 +0530

    5th program

---
+++ ex7.py
@@ -0,0 +1,16 @@
+end1 = 'R'
+end2 = 'a'
+end3 = 'j'
+end4 = 'p'
+end5 = 'u'
+end6 = 't'
+
+
+
+print "." * 10
+print " adhunik is my name."
+print " Surname is "
+print end1+end2+end3+end4+end5+end6 
+print " my height is %s" %'black'
+print "." * 10
+
