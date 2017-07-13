06a3ba3ca6eb466e72b924f006d21a03d1fbdf9d
adhunik13 <adhunik13@yahoo.com>
2017-07-13 23:20:59 +0530

    11th program

---
+++ ex11.py
@@ -0,0 +1,16 @@
+#using Math symbol
+print "TIME TO DO SOME CALCULATIONS "
+print
+print  "enter 1st number"
+x = int(raw_input())
+print  "enter 2nd number"
+y = int(raw_input())
+print"." * 15
+print x+y
+print"." * 15
+
+
+#int(raw_input())
+#to mke user enter numbers  
+
+ 
