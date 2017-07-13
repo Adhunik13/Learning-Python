675a8ef18b54b2fee6c000ef4df8ab968a0d0d36
adhunik13 <adhunik13@yahoo.com>
2017-07-13 22:16:04 +0530

    3rd program

---
+++ ex5.py
@@ -0,0 +1,14 @@
+# number has %d and string is %s
+name = 'Adhunik Rajput'
+age = 20
+height = 180 #cm
+eyes = 'black'
+teeth = 'white'
+hair = 'black'
+
+print "Let's talk abut %s" %name
+print "my eyes color is %s" %eyes
+print "my hair color is %s" %hair
+print "teeth are %s " %teeth
+
+print "if i add %d, %d I get %d. "%(age,height,age+height)
