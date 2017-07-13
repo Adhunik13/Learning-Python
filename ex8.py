15e03ff21409cd967c039d7afe7e9f9b177cfd9d
adhunik13 <adhunik13@yahoo.com>
2017-07-13 23:20:34 +0530

    8th program

---
+++ ex8.py
@@ -0,0 +1,8 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug"
+
+print "here are the days: ", days 
+print "here are the months: ", months
+
+
+print""" these  three double-quotes are use to write more than 1 line and it really works """
